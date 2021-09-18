dee705146ab757d061df72d310065530efc7afe1
shoei.kanno <kanno.shoei@gmail.com>
2021-09-18 12:39:49 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/demo/NaitoProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaitoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaitoProjectApplication.class, args);
+		System.out.println("sssss");
+	}
+
+}
